80299b662f5dab7dfd60223e0ce7e085916cc76a
ArcAnc <ArcAnc13@gmail.com>
2026-08-14 05:40:12 +0300

    Optimize GPU deformer texel fetches

--- src/main/java/com/arcanc/pulselib/util/PRenderTypes.java
+++ src/main/java/com/arcanc/pulselib/util/PRenderTypes.java
@@ -45,8 +45,8 @@ public class PRenderTypes
 				withSampler("Sampler2").
 				withUniform("Lighting", UniformType.UNIFORM_BUFFER).
 				withUniform("InstanceData", UniformType.UNIFORM_BUFFER).
-				withUniform("DeformerOperations", UniformType.TEXEL_BUFFER, GpuFormat.R8_SINT).
-				withUniform("DeformerValues", UniformType.TEXEL_BUFFER, GpuFormat.R8_SINT).
+				withUniform("DeformerOperations", UniformType.TEXEL_BUFFER, GpuFormat.R32_FLOAT).
+				withUniform("DeformerValues", UniformType.TEXEL_BUFFER, GpuFormat.R32_FLOAT).
 				build();
 		private static final BindGroupLayout TRIANGLES_GUI_LAYOUT = BindGroupLayout.builder().
 				withUniform("ColorOverlay", UniformType.UNIFORM_BUFFER).
